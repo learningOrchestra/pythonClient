64c1735e2d6704d456cf7d3a1dfd67702d1f9fa8
Matheus-Gr <matheus.gr@aluno.ufop.edu.br>
2020-11-11 20:57:15 -0300

    refactoring and updates
    was added pretty response method,
    return on each methods,
    changes of nomenclatures,
    update on documentation

--- learning_orchestra_client/dataset/dataset.py
+++ learning_orchestra_client/dataset/dataset.py
@@ -1,10 +1,8 @@
 """
 
 """
-__author__ = "Otávio Henrique Rodrigues Mapa & Matheus Gonçalves Ribeiro"
+__author__ = "Otavio Henrique Rodrigues Mapa & Matheus Goncalves Ribeiro"
 __credits__ = "all free source developers"
-__license__ = "GNU General Public License"
-__version__ = "1.0.0"
 __status__ = "Prototype"
 
 import requests
@@ -17,39 +15,89 @@ cluster_url = None
 class Dataset:
 
     def __init__(self, ip_from_cluster):
-        global cluster_url
-        self.cluster_url = "http://" + ip_from_cluster
+        # global cluster_url
+        self.cluster_url = "http://" + ip_from_cluster + "/api/learningOrchestra/v1/dataset"
 
-    def post(self, dataset_name, url):
-        """ description: download a file on your cluster
-        dataset_name: name of file
-        url: url to CSV file
+    @staticmethod
+    def pretty_response(response):
         """
-        cluster_url_dataset = self.cluster_url + "/api/learningOrchestra/v1/dataset"
+        description: This method is responsible to return an indented json file.
+
+        parsed: Json file that will be indented.
+
+        return: Indented json file.
+        """
+        parsed = response
+        return json.dumps(parsed, indent=4, sort_keys=True)
+
+    # def insert_dataset_async(self, dataset_name, url):
+
+    def insert_dataset_sync(self, dataset_name, url):
+        """
+        description: This method is responsible to insert a dataset from a URI asynchronously, i.e., the caller does
+        not wait until the dataset is inserted into the Learning Orchestra storage mechanism. Instead, the caller
+        receives a JSON object with a URL to proceed future calls to verify if the dataset is inserted.
+
+        dataset_name: Is the name of the dataset file that will be created.
+        url: Url to CSV file.
+
+        return: A JSON object with an error or warning message or a URL indicating the correct operation (the caller
+        must use such an URL to proceed future checks to verify if the dataset is inserted).
+        """
+        cluster_url_dataset = self.cluster_url
         request_body = {"datasetName": dataset_name, "url": url}
         response = requests.post(url=cluster_url_dataset, json=request_body)
         print("\n----------" + " CREATE FILE " + dataset_name + " ----------")
-        print(response.json())
+        response = self.pretty_response(response.json())
+        return response
 
-    def get_dataset_list(self):
-        """ description: show a list with dataset file names
+    def search_all_datasets(self):
         """
-        cluster_url_dataset = self.cluster_url + "/api/learningOrchestra/v1/dataset"
+        description: This method retrieves all datasets metadata, i.e., it does not retrieve the dataset content.
+        dataset_name: Is the name of the dataset file.
+
+        return: All datasets metadata stored in Learning Orchestra or an empty result.
+        """
+        cluster_url_dataset = self.cluster_url
         response = requests.get(cluster_url_dataset)
-        print(response.json())
+        response = self.pretty_response(response.json())
+        return response
 
-    def get_dataset(self, dataset_name):
-        """ description: read a dataset
-        dataset_name: name of file
+    # def search_dataset(self):
+
+    def search_dataset_content(self, dataset_name, query, limit, skip):
         """
-        cluster_url_dataset = self.cluster_url + "/api/learningOrchestra/v1/dataset/" + dataset_name + "?query={}&limit=5&skip=0"
+        description:  This method is responsible for retrieving the dataset content
+
+        dataset_name: Is the name of the dataset file.
+        query: Query to make in MongoDB(default: empty query)
+        limit: Number of rows to return in pagination(default: 10) (maximum is set at 20 rows per request)
+        skip: Number of rows to skip in pagination(default: 0)
+
+        return A page with some tuples or registers inside or an error if there is no such dataset. The current page
+        is also returned to be used in future content requests.
+        """
+        cluster_url_dataset = self.cluster_url + "/" + dataset_name + "?query=" + query + "&limit=" + str(
+            limit) + "&skip=" + str(skip)
         response = requests.get(cluster_url_dataset)
-        print(response.json())
+        response = self.pretty_response(response.json())
+        return response
 
-    def delete(self, dataset_name):
-        """ description: delete a dataset
-            dataset_name: name of file
+    # def update_dataset_sync(self, dataset_name, data_type:
+
+    # def update_dataset_async(self, dataset_name, data_type:
+
+    def delete_dataset(self, dataset_name):
         """
-        cluster_url_dataset = self.cluster_url + "/api/learningOrchestra/v1/dataset/" + dataset_name
+        description: This method is responsible for deleting the dataset. The delete operation is always synchronous
+        because it is very fast, since the deletion is performed in background. If a dataset was used by another task
+        (Ex. projection, histogram, pca, tuning and so forth), it cannot be deleted.
+
+        dataset_name: Represents the dataset name.
+
+        return: JSON object with an error message, a warning message or a correct delete message
+        """
+        cluster_url_dataset = self.cluster_url + "/" + dataset_name
         response = requests.delete(cluster_url_dataset)
-        print(response.json())
+        response = self.pretty_response(response.json())
+        return response
--- learning_orchestra_client/main.py
+++ learning_orchestra_client/main.py
@@ -1,11 +1,12 @@
-from client.dataset.dataset import Dataset
+from dataset.dataset import Dataset
 
 ip_from_cluster = "35.247.250.122"
 dataset_name = "train_02"
-url = "https://filebin.net/cudw93up9y2jsizg/train.xls?t=jgc5pksx"
+url = "https://filebin.net/cudw93up9y2jsizg/train.xls?t=igt76bj6"
 
 p = Dataset(ip_from_cluster)
-#p.post(dataset_name, url)
-p.get_dataset_list()
-#p.get_dataset(dataset_name)
-#p.delete(dataset_name)
+# just_print = p.insert_dataset_sync(dataset_name, url)
+just_print = p.search_all_datasets()
+# just_print = p.search_dataset_content(dataset_name, "{}", limit=0, skip=0)
+# just_print = p.delete_dataset(dataset_name)
+print(just_print)
