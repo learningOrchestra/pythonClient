41a10c297a04439c846b46987b77b9fd9b2d2932
Matheus-Gr <matheus.gr@aluno.ufop.edu.br>
2020-11-14 13:48:32 -0300

    corrections on dataset, and methods created on projection

--- learning_orchestra_client/dataset/dataset.py
+++ learning_orchestra_client/dataset/dataset.py
@@ -5,12 +5,11 @@ __author__ = "Otavio Henrique Rodrigues Mapa & Matheus Goncalves Ribeiro"
 __credits__ = "all free source developers"
 __status__ = "Prototype"
 
+from response_treat import ResponseTreat
 import requests
 import json
 import time
 import ast
-
-from response_treat import ResponseTreat
 
 
 class Dataset:
@@ -18,30 +17,30 @@ class Dataset:
     def __init__(self, ip_from_cluster):
         self.cluster_url = "http://" + ip_from_cluster + "/api/learningOrchestra/v1/dataset"
 
-    def insert_dataset_sync(self, dataset_name, url, pretty_response=True):
+    def insert_dataset_sync(self, dataset_name, url, pretty_response=False):
         """
         description: This method is responsible to insert a dataset from a URI asynchronously, i.e., the caller wait
         until the last dataset is inserted into the Learning Orchestra storage mechanism to insert other dataset.
 
-        waiting: Responsible to block other insert until last instead was done
+        waiting: Responsible to block other insert until last insert was done
         dataset_name: Is the name of the dataset file that will be created.
         url: Url to CSV file.
 
         return: A JSON object with an error or warning message or a URL indicating the correct operation (the caller
         must use such an URL to proceed future checks to verify if the dataset is inserted).
         """
-        waiting = False
-        waiting = self.its_not_ready()
-        if waiting:
+        waiting = self.its_ready()
+        if not waiting:
             print("waiting for the last dataset to be processed")
         else:
             cluster_url_dataset = self.cluster_url
             request_body = {"datasetName": dataset_name, "url": url}
             response = requests.post(url=cluster_url_dataset, json=request_body)
-            print("\n----------" + " CREATE FILE " + dataset_name + " ----------")
+            if pretty_response:
+                print("\n----------" + " CREATE FILE " + dataset_name + " ----------")
             return ResponseTreat().treatment(response, pretty_response)
 
-    def insert_dataset_async(self, dataset_name, url, pretty_response=True):
+    def insert_dataset_async(self, dataset_name, url, pretty_response=False):
         """
         description: This method is responsible to insert a dataset from a URI asynchronously, i.e., the caller does
         not wait until the dataset is inserted into the Learning Orchestra storage mechanism. Instead, the caller
@@ -59,10 +58,22 @@ class Dataset:
         print("\n----------" + " CREATE FILE " + dataset_name + " ----------")
         return ResponseTreat().treatment(response, pretty_response)
 
-    def search_all_datasets(self, pretty_response=True):
+    def search_dataset(self, dataset_name, pretty_response=False):
+        """
+        description: This method is responsible for retrieving a specific dataset
+
+        dataset_name: Is the name of the dataset file.
+        limit: Number of rows to return in pagination(default: 10) (maximum is set at 20 rows per request)
+        skip: Number of rows to skip in pagination(default: 0)
+
+        return Specific dataset metadata stored in Learning Orchestra or an error if there is no such dataset.
+        """
+        response = self.search_dataset_content(dataset_name, limit=1, pretty_response=pretty_response)
+        return response
+
+    def search_all_datasets(self, pretty_response=False):
         """
         description: This method retrieves all datasets metadata, i.e., it does not retrieve the dataset content.
-        dataset_name: Is the name of the dataset file.
 
         return: All datasets metadata stored in Learning Orchestra or an empty result.
         """
@@ -70,15 +81,7 @@ class Dataset:
         response = requests.get(cluster_url_dataset)
         return ResponseTreat().treatment(response, pretty_response)
 
-    def search_dataset(self, dataset_name, pretty_response=True):
-        response = self.search_dataset_content(dataset_name, limit=1, pretty_response=pretty_response)
-        return response
-
-    # def update_dataset_sync(self, dataset_name, data_type:
-
-    # def update_dataset_async(self, dataset_name, data_type:
-
-    def search_dataset_content(self, dataset_name, query="{}", limit=0, skip=0, pretty_response=True):
+    def search_dataset_content(self, dataset_name, query="{}", limit=0, skip=0, pretty_response=False):
         """
         description:  This method is responsible for retrieving the dataset content
 
@@ -95,7 +98,7 @@ class Dataset:
         response = requests.get(cluster_url_dataset)
         return ResponseTreat().treatment(response, pretty_response)
 
-    def delete_dataset(self, dataset_name, pretty_response=True):
+    def delete_dataset(self, dataset_name, pretty_response=False):
         """
         description: This method is responsible for deleting the dataset. The delete operation is always synchronous
         because it is very fast, since the deletion is performed in background. If a dataset was used by another task
@@ -109,7 +112,11 @@ class Dataset:
         response = requests.delete(cluster_url_dataset)
         return ResponseTreat().treatment(response, pretty_response)
 
-    def its_not_ready(self):
+    # def update_dataset_sync(self, dataset_name, data_type:
+
+    # def update_dataset_async(self, dataset_name, data_type:
+
+    def its_ready(self):
         """
         description: This method check if all datasets has finished being inserted into the Learning Orchestra storage
         mechanism.
--- learning_orchestra_client/transform/projection.py
+++ learning_orchestra_client/transform/projection.py
@@ -1,4 +1,93 @@
-class Projection:
-    """
+"""
 
-    """
+"""
+from response_treat import ResponseTreat
+import requests
+
+
+class Projection:
+    def __init__(self, ip_from_cluster):
+        self.cluster_url = "http://" + ip_from_cluster + "/api/learningOrchestra/v1/transform/projection"
+
+    # def delete_dataset_attributes_sync(self):
+
+    # def insert_dataset_attributes_sync(self):
+
+    # def insert_dataset_attribute_sync(self):
+
+    # def reduce_dataset_sync(self):
+
+    # def enlarge_dataset_sync(self):
+
+    # def join_datasets_sync(self):
+
+    # def join_dataset_sync(self):
+
+    # def update_dataset_values_sync(self):
+
+    def search_projections(self, projection_name, pretty_response=False):
+        """
+        description:  This method is responsible for retrieving a specific projection
+
+        dataset_name: Is the name of the dataset file.
+        limit: Number of rows to return in pagination(default: 10) (maximum is set at 20 rows per request)
+        skip: Number of rows to skip in pagination(default: 0)
+
+        return: Specific projection metadata stored in Learning Orchestra or an error if there is no such projections.
+        """
+        response = self.search_projections_content(projection_name, limit=1, pretty_response=pretty_response)
+        return response
+
+    def search_all_projections(self, pretty_response=False):
+        """
+        description: This method retrieves all projection metadata, it does not retrieve the projection content.
+
+        return: All projections metadata stored in Learning Orchestra or an empty result.
+        """
+        cluster_url_projection = self.cluster_url
+        response = requests.get(cluster_url_projection)
+        return ResponseTreat().treatment(response, pretty_response)
+
+    def search_projections_content(self, projection_name, query="{}", limit=0, skip=0, pretty_response=True):
+        """
+        description: This method is responsible for retrieving the dataset content
+
+        dataset_name: Is the name of the dataset file.
+        query: Query to make in MongoDB(default: empty query)
+        limit: Number of rows to return in pagination(default: 10) (maximum is set at 20 rows per request)
+        skip: Number of rows to skip in pagination(default: 0)
+
+        return A page with some tuples or registers inside or an error if there is no such dataset. The current page
+        is also returned to be used in future content requests.
+        """
+        cluster_url_projection = self.cluster_url + "/" + projection_name + "?query=" + query + "&limit=" + str(
+            limit) + "&skip=" + str(skip)
+        response = requests.get(cluster_url_projection)
+        return ResponseTreat().treatment(response, pretty_response)
+
+    def delete_projections(self, projection_name, pretty_response=False):
+        """
+        description: This method is responsible for deleting the projection. The delete operation is always synchronous
+        because it is very fast, since the deletion is performed in background. If a projection was used by another task
+        (Ex. histogram, pca, tuning and so forth), it cannot be deleted.
+
+        projection_name: Represents the projection name.
+
+        return: JSON object with an error message, a warning message or a correct delete message
+        """
+        cluster_url_projection = self.cluster_url + "/" + projection_name
+        response = requests.delete(cluster_url_projection)
+        return ResponseTreat().treatment(response, pretty_response)
+
+    def its_ready(self):
+        """
+        description: This method check if all datasets has finished being inserted into the Learning Orchestra storage
+        mechanism.
+        """
+        operable = self.search_all_projections()
+        if '"finished": false' in operable:
+            return True
+
+
+
+
