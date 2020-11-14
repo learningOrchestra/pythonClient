42a7886bb9ffaa69d1a897e976ca02a64f779285
otaviomapa <otavio.mapa@gmail.com>
2020-11-14 17:33:20 -0300

    little bugs fixed and reformatting code

--- learning_orchestra_client/dataset/dataset.py
+++ learning_orchestra_client/dataset/dataset.py
@@ -13,43 +13,50 @@ import time
 class Dataset:
 
     def __init__(self, ip_from_cluster):
-        self.cluster_url = "http://" + ip_from_cluster + "/api/learningOrchestra/v1/dataset"
-        self.WAIT_TIME = 10
+        self.cluster_url = "http://" + ip_from_cluster + "/api/" \
+                            "learningOrchestra/v1/dataset"
+        self.WAIT_TIME = 3
         self.METADATA_INDEX = 0
 
     def insert_dataset_sync(self, dataset_name, url, pretty_response=False):
         """
-        description: This method is responsible to insert a dataset from a URI synchronously, i.e., the caller waits
-        until the dataset is inserted into the Learning Orchestra storage mechanism.
+        description: This method is responsible to insert a dataset from a URI
+        synchronously, i.e., the caller waits until the dataset is inserted into
+        the Learning Orchestra storage mechanism.
 
         pretty_response: If true return indented string, else return dict.
         waiting: Responsible to block other insert until last insert was done
         dataset_name: Is the name of the dataset file that will be created.
         url: Url to CSV file.
 
-        return: A JSON object with an error or warning message or a URL indicating the correct operation (the caller
-        must use such an URL to proceed future checks to verify if the dataset is inserted).
+        return: A JSON object with an error or warning message or a URL
+        indicating the correct operation (the caller must use such an URL to
+        proceed future checks to verify if the dataset is inserted).
         """
         cluster_url_dataset = self.cluster_url
         request_body = {"datasetName": dataset_name, "url": url}
         response = requests.post(url=cluster_url_dataset, json=request_body)
-        self.its_ready(dataset_name, pretty_response)
+        self.verify_dataset_processing_done(dataset_name, pretty_response)
         if pretty_response:
-            print("\n----------" + " CREATED FILE " + dataset_name + " ----------")
+            print("\n----------" + " CREATED FILE " + dataset_name + " -------"
+                                                                     "---")
         return ResponseTreat().treatment(response, pretty_response)
 
     def insert_dataset_async(self, dataset_name, url, pretty_response=False):
         """
-        description: This method is responsible to insert a dataset from a URI asynchronously, i.e., the caller does
-        not wait until the dataset is inserted into the Learning Orchestra storage mechanism. Instead, the caller
-        receives a JSON object with a URL to proceed future calls to verify if the dataset is inserted.
+        description: This method is responsible to insert a dataset from a URI
+        asynchronously, i.e., the caller does not wait until the dataset is
+        inserted into the Learning Orchestra storage mechanism. Instead, the
+        caller receives a JSON object with a URL to proceed future calls to
+        verify if the dataset is inserted.
 
         pretty_response: If true return indented string, else return dict.
         dataset_name: Is the name of the dataset file that will be created.
         url: Url to CSV file.
 
-        return: A JSON object with an error or warning message or a URL indicating the correct operation (the caller
-        must use such an URL to proceed future checks to verify if the dataset is inserted).
+        return: A JSON object with an error or warning message or a URL
+        indicating the correct operation (the caller must use such an URL to
+        proceed future checks to verify if the dataset is inserted).
         """
         cluster_url_dataset = self.cluster_url
         request_body = {"datasetName": dataset_name, "url": url}
@@ -59,58 +66,74 @@ class Dataset:
 
     def search_dataset(self, dataset_name, pretty_response=False):
         """
-        description: This method is responsible for retrieving a specific dataset
+        description: This method is responsible for retrieving a specific
+        dataset
 
         pretty_response: If true return indented string, else return dict.
         dataset_name: Is the name of the dataset file.
-        limit: Number of rows to return in pagination(default: 10) (maximum is set at 20 rows per request)
+        limit: Number of rows to return in pagination(default: 10) (maximum is
+        set at 20 rows per request)
         skip: Number of rows to skip in pagination(default: 0)
 
-        return Specific dataset metadata stored in Learning Orchestra or an error if there is no such dataset.
+        return Specific dataset metadata stored in Learning Orchestra or an
+        error if there is no such dataset.
         """
-        response = self.search_dataset_content(dataset_name, limit=1, pretty_response=pretty_response)
+        response = self.search_dataset_content(dataset_name, limit=1,
+                                               pretty_response=pretty_response)
         return response
 
     def search_all_datasets(self, pretty_response=False):
         """
-        description: This method retrieves all datasets metadata, i.e., it does not retrieve the dataset content.
+        description: This method retrieves all datasets metadata, i.e., it does
+        not retrieve the dataset content.
 
         pretty_response: If true return indented string, else return dict.
 
-        return: All datasets metadata stored in Learning Orchestra or an empty result.
+        return: All datasets metadata stored in Learning Orchestra or an empty
+        result.
         """
         cluster_url_dataset = self.cluster_url
         response = requests.get(cluster_url_dataset)
         return ResponseTreat().treatment(response, pretty_response)
 
-    def search_dataset_content(self, dataset_name, query="{}", limit=0, skip=0, pretty_response=False):
+    def search_dataset_content(self, dataset_name, query={}, limit=10, skip=0,
+                               pretty_response=False):
         """
-        description:  This method is responsible for retrieving the dataset content
+        description:  This method is responsible for retrieving the dataset
+        content
 
         pretty_response: If true return indented string, else return dict.
         dataset_name: Is the name of the dataset file.
         query: Query to make in MongoDB(default: empty query)
-        limit: Number of rows to return in pagination(default: 10) (maximum is set at 20 rows per request)
+        limit: Number of rows to return in pagination(default: 10) (maximum is
+        set at 20 rows per request)
         skip: Number of rows to skip in pagination(default: 0)
 
-        return A page with some tuples or registers inside or an error if there is no such dataset. The current page
-        is also returned to be used in future content requests.
+        return A page with some tuples or registers inside or an error if there
+        is no such dataset. The current page is also returned to be used in
+        future content requests.
         """
-        cluster_url_dataset = self.cluster_url + "/" + dataset_name + "?query=" + query + "&limit=" + str(
-            limit) + "&skip=" + str(skip)
+        if query is None:
+            query = {}
+        cluster_url_dataset = self.cluster_url + "/" + dataset_name + "?query="\
+                              + str(query) + "&limit=" + str(limit) +\
+                              "&skip=" + str(skip)
         response = requests.get(cluster_url_dataset)
         return ResponseTreat().treatment(response, pretty_response)
 
     def delete_dataset(self, dataset_name, pretty_response=False):
         """
-        description: This method is responsible for deleting the dataset. The delete operation is always synchronous
-        because it is very fast, since the deletion is performed in background. If a dataset was used by another task
-        (Ex. projection, histogram, pca, tuning and so forth), it cannot be deleted.
+        description: This method is responsible for deleting the dataset. The
+        delete operation is always synchronous because it is very fast, since
+        the deletion is performed in background. If a dataset was used by
+        another task (Ex. projection, histogram, pca, tuning and so forth), it
+        cannot be deleted.
 
         pretty_response: If true return indented string, else return dict.
         dataset_name: Represents the dataset name.
 
-        return: JSON object with an error message, a warning message or a correct delete message
+        return: JSON object with an error message, a warning message or a
+        correct delete message
         """
         cluster_url_dataset = self.cluster_url + "/" + dataset_name
         response = requests.delete(cluster_url_dataset)
@@ -120,9 +143,11 @@ class Dataset:
 
     # def update_dataset_async(self, dataset_name, data_type:
 
-    def its_ready(self, dataset_name, pretty_response=True):
+    def verify_dataset_processing_done(self, dataset_name,
+                                        pretty_response=True):
         """
-        description: This method check from time to time using Time lib, if a dataset has finished being inserted
+        description: This method check from time to time using Time lib, if a
+        dataset has finished being inserted
         into the Learning Orchestra storage mechanism.
 
         pretty_response: If true return indented string, else return dict.
@@ -131,7 +156,8 @@ class Dataset:
             print("\n---------- WAITING " + dataset_name + " FINISH ----------")
         while True:
             time.sleep(self.WAIT_TIME)
-            response = self.search_dataset_content(dataset_name, limit=1, pretty_response=False)
+            response = self.search_dataset_content(dataset_name, limit=1,
+                                                   pretty_response=False)
             if len(response["result"]) == 0:
                 continue
             if response["result"][self.METADATA_INDEX]["finished"]:
--- learning_orchestra_client/transform/projection.py
+++ learning_orchestra_client/transform/projection.py
@@ -9,7 +9,10 @@ import time
 
 class Projection:
     def __init__(self, ip_from_cluster):
-        self.cluster_url = "http://" + ip_from_cluster + "/api/learningOrchestra/v1/transform/projection"
+        self.cluster_url = "http://" + ip_from_cluster + "/api/" \
+                            "learningOrchestra/v1/transform/projection"
+        self.WAIT_TIME = 3
+        self.METADATA_INDEX = 0
 
     # def delete_dataset_attributes_sync(self):
 
@@ -29,90 +32,112 @@ class Projection:
 
     def search_projections(self, projection_name, pretty_response=False):
         """
-        description:  This method is responsible for retrieving a specific projection
+        description:  This method is responsible for retrieving a specific
+        projection
 
         pretty_response: If true return indented string, else return dict.
         dataset_name: Is the name of the dataset file.
-        limit: Number of rows to return in pagination(default: 10) (maximum is set at 20 rows per request)
+        limit: Number of rows to return in pagination(default: 10) (maximum is
+        set at 20 rows per request)
         skip: Number of rows to skip in pagination(default: 0)
 
-        return: Specific projection metadata stored in Learning Orchestra or an error if there is no such projections.
+        return: Specific projection metadata stored in Learning Orchestra or an
+        error if there is no such projections.
         """
-        response = self.search_projections_content(projection_name, limit=1, pretty_response=pretty_response)
+        response = self.search_projections_content(projection_name, limit=1,
+                                                   pretty_response=
+                                                        pretty_response)
         return response
 
     def search_all_projections(self, pretty_response=False):
         """
-        description: This method retrieves all projection metadata, it does not retrieve the projection content.
+        description: This method retrieves all projection metadata, it does not
+        retrieve the projection content.
 
         pretty_response: If true return indented string, else return dict.
 
-        return: All projections metadata stored in Learning Orchestra or an empty result.
+        return: All projections metadata stored in Learning Orchestra or an
+        empty result.
         """
         cluster_url_projection = self.cluster_url
         response = requests.get(cluster_url_projection)
         return ResponseTreat().treatment(response, pretty_response)
 
-    def search_projections_content(self, projection_name, query="{}", limit=0, skip=0, pretty_response=True):
+    def search_projections_content(self, projection_name, query={}, limit=10,
+                                   skip=0, pretty_response=False):
         """
-        description: This method is responsible for retrieving the dataset content
+        description: This method is responsible for retrieving the dataset
+        content
 
         pretty_response: If true return indented string, else return dict.
         dataset_name: Is the name of the dataset file.
         query: Query to make in MongoDB(default: empty query)
-        limit: Number of rows to return in pagination(default: 10) (maximum is set at 20 rows per request)
+        limit: Number of rows to return in pagination(default: 10) (maximum is
+        set at 20 rows per request)
         skip: Number of rows to skip in pagination(default: 0)
 
-        return A page with some tuples or registers inside or an error if there is no such dataset. The current page
-        is also returned to be used in future content requests.
+        return A page with some tuples or registers inside or an error if there
+        is no such dataset. The current page is also returned to be used in
+        future content requests.
         """
-        cluster_url_projection = self.cluster_url + "/" + projection_name + "?query=" + query + "&limit=" + str(
-            limit) + "&skip=" + str(skip)
+        cluster_url_projection = self.cluster_url + "/" + projection_name \
+                                 + "?query=" + str(query) + "&limit=" + str(
+                                    limit) + "&skip=" + str(skip)
         response = requests.get(cluster_url_projection)
         return ResponseTreat().treatment(response, pretty_response)
 
     def delete_projections(self, projection_name, pretty_response=False):
         """
-        description: This method is responsible for deleting the projection. The delete operation is always synchronous
-        because it is very fast, since the deletion is performed in background. If a projection was used by another task
+        description: This method is responsible for deleting the projection.
+        The delete operation is always synchronous because it is very fast,
+        since the deletion is performed in background. If a projection was used
+        by another task
         (Ex. histogram, pca, tuning and so forth), it cannot be deleted.
 
         pretty_response: If true return indented string, else return dict.
         projection_name: Represents the projection name.
 
-        return: JSON object with an error message, a warning message or a correct delete message
+        return: JSON object with an error message, a warning message or a
+        correct delete message
         """
         cluster_url_projection = self.cluster_url + "/" + projection_name
         response = requests.delete(cluster_url_projection)
         return ResponseTreat().treatment(response, pretty_response)
 
-    def its_ready(self, projection_name, pretty_response=True):
+    def verify_projection_processing_done(self, projection_name,
+                                          pretty_response=False):
         """
-        description: This method check from time to time using Time lib, if a projection has finished being inserted
-        into the Learning Orchestra storage mechanism.
+        description: This method check from time to time using Time lib, if a
+        projection has finished being inserted into the Learning Orchestra
+        storage mechanism.
 
         pretty_response: If true return indented string, else return dict.
         """
         if pretty_response:
-            print("\n---------- WAITING " + projection_name + " FINISH ----------")
+            print(
+                "\n---------- WAITING " + projection_name + " FINISH -------"
+                                                            "---")
         while True:
             time.sleep(self.WAIT_TIME)
-            response = self.search_projections_content(projection_name, limit=1, pretty_response=False)
+            response = self.search_projections_content(projection_name, limit=1,
+                                                       pretty_response=False)
             if len(response["result"]) == 0:
                 continue
             if response["result"][self.METADATA_INDEX]["finished"]:
                 break
 
-    def insert_dataset_attributes_async(self, dataset_name, projection_name, fields, pretty_response=False):
+    def insert_dataset_attributes_async(self, dataset_name, projection_name,
+                                        fields, pretty_response=False):
         """
-        description: This method inserts a set of attributes into a dataset. It can create a new dataset or reuse the
-        existing one.
+        description: This method inserts a set of attributes into a dataset. It
+        can create a new dataset or reuse the existing one.
 
         pretty_response: If true return indented string, else return dict.
         datasetName: Represents the dataset name.
         fields: Represents the set of attributes to be inserted.
 
-        return: A JSON object with error or warning messages. In case of success, it returns the dataset metadata.
+        return: A JSON object with error or warning messages. In case of
+        success, it returns the dataset metadata.
         """
         if pretty_response:
             print(
@@ -128,11 +153,7 @@ class Projection:
             "outputDatasetName": projection_name,
             "names": fields,
         }
-        Dataset.its_ready(dataset_name, pretty_response)
+        Dataset.verify_dataset_processing_done(dataset_name, pretty_response)
         request_url = self.cluster_url
         response = requests.post(url=request_url, json=request_body)
         return ResponseTreat().treatment(response, pretty_response)
-
-
-
-
