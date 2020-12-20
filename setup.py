eec80d1a252d583dd116c6d3b8661274a4d5903e
Gabriel Ribeiro <gabbriel.rribeiro@gmail.com>
2020-12-20 13:12:38 -0300

    update docs

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name="learning_orchestra_client",
     version="2.0.0",
     author="Gabriel Ribeiro",
     author_email="gabbriel.rribeiro@gmail.com",
     description="learningOrchestra python client",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/learningOrchestra/learningOrchestra-python-client",
     packages=setuptools.find_packages(),
     classifiers=[
