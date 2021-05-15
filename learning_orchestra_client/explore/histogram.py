f80dbdf609320ffcb87d4675b9755f448dfadb40
riibeirogabriel <gabbriel.rribeiro@gmail.com>
2021-05-15 18:14:21 -0300

    fixes

--- learning_orchestra_client/explore/histogram.py
+++ learning_orchestra_client/explore/histogram.py
@@ -1,8 +1,8 @@
 from learning_orchestra_client.observe.observe import Observer
-from learning_orchestra_client.util._response_treat import ResponseTreat
+from learning_orchestra_client._util._response_treat import ResponseTreat
 import requests
 from typing import Union
-from learning_orchestra_client.util._entity_reader import EntityReader
+from learning_orchestra_client._util._entity_reader import EntityReader
 
 
 class ExploreHistogram:
