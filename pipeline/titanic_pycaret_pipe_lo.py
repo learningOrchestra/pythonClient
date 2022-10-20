4cd28208fc348d8182520b3c4dd3abb1fee59552
Jonatas Miguel <jonatas_miguel@hotmail.com>
2022-10-20 20:59:34 -0300

    Update titanic_pycaret_pipe_lo.py

--- pipeline/titanic_pycaret_pipe_lo.py
+++ pipeline/titanic_pycaret_pipe_lo.py
@@ -1,7 +1,7 @@
 from learning_orchestra_client.builder.builder import BuilderPycaret
 from learning_orchestra_client.dataset.csv import DatasetCsv
 
-CLUSTER_IP = "http://34.125.23.29"
+CLUSTER_IP = "http://34.151.200.12"
 
 dataset_csv = DatasetCsv(CLUSTER_IP)
 dataset_csv.delete_dataset("train")
@@ -12,7 +12,7 @@ dataset_csv.insert_dataset_async(
 dataset_csv.wait(dataset_name="train",
                  timeout=1)
 
-version = 3012
+version = 5
 name = f'titanic{version}'
 codigo = """
 from pycaret import classification
@@ -33,17 +33,16 @@ response = None
 """
 builder = BuilderPycaret(CLUSTER_IP)
 
-# builder.run_pycaret_async(
-#     name=name,
-#     parameters={
-#          "train": "$train"
-#     },
-#     code=codigo)
-# builder.wait(name, 1)
-#
-# print(builder.search_execution_content(
-#     name=name,
-#     pretty_response=True))
+builder.run_pycaret_async(
+    name=name,
+    parameters={
+         "train": "$train"
+    },
+    code=codigo)
+builder.wait(name, 1)
+print(builder.search_execution_content(
+    name=name,
+    pretty_response=True))
 
 print(builder.search_report(file_name='report_tuned'))
 
